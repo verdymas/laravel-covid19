6e45a8e89baf5504a0c8fafc40780ea8a8dcdbf1
verdymas <atmaverdymas@gmail.com>
2021-06-09 19:38:18 +0700

    Add Icon

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,10 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin.home') }}" class="brand-link">
-        <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3"
-             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="{{ asset('adminlte/dist/img/icon.png') }}" alt="App Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">LaraCOV19</span>
     </a>
 
     <!-- Sidebar -->
@@ -13,19 +12,22 @@
         <div id="user-panel-container">
             <div class="user-panel mt-3 pb-3 mb-3 d-flex" id="user-panel">
                 <div class="image">
-                    <?php
-                    if (auth()->guard('admin')->user()->img_adm != '') {
-                        $pp_adm = auth()->guard('admin')->user()->img_adm;
+                    <?php if (
+                    auth()
+                    ->guard('admin')
+                    ->user()->img_adm != ''
+                    ) {
+                    $pp_adm = auth()
+                    ->guard('admin')
+                    ->user()->img_adm;
                     } else {
-                        $pp_adm = 'default-photo.png';
-                    }
-                    ?>
+                    $pp_adm = 'default-photo.png';
+                    } ?>
                     <img src="{{ asset('adminlte/avatar/' . $pp_adm) }}" class="img-circle elevation-2"
-                         alt="User Image">
+                        alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="{{-- route('account.index') --}}"
-                       class="d-block">{{ auth()->guard('admin')->user()->nm_adm }}</a>
+                    <a href="{{-- route('account.index') --}}" class="d-block">{{ auth()->guard('admin')->user()->nm_adm }}</a>
                 </div>
             </div>
         </div>
@@ -36,7 +38,7 @@
                 <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                 <li class="nav-item">
                     <a href="{{ route('admin.home') }}"
-                       class="nav-link {{ request()->routeIs('admin.home*') ? 'active' : '' }}">
+                        class="nav-link {{ request()->routeIs('admin.home*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-home"></i>
                         <p>
                             Home
@@ -54,21 +56,21 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="{{ route('kartu-keluarga.index') }}"
-                               class="nav-link {{ request()->routeIs('kartu-keluarga*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->routeIs('kartu-keluarga*') ? 'active' : '' }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Kartu Keluarga</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('warga.index') }}"
-                               class="nav-link {{ request()->routeIs('warga*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->routeIs('warga*') ? 'active' : '' }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Warga</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('satgas.index') }}"
-                               class="nav-link {{ request()->routeIs('satgas*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->routeIs('satgas*') ? 'active' : '' }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Satgas</p>
                             </a>
@@ -77,7 +79,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('bantuan.index') }}"
-                       class="nav-link {{ request()->routeIs('bantuan*') ? 'active' : '' }}">
+                        class="nav-link {{ request()->routeIs('bantuan*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-atlas"></i>
                         <p>
                             Bantuan
@@ -86,7 +88,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('histori.index') }}"
-                       class="nav-link {{ request()->routeIs('histori*') ? 'active' : '' }}">
+                        class="nav-link {{ request()->routeIs('histori*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-book-medical"></i>
                         <p>
                             Histori
@@ -95,8 +97,7 @@
                 </li>
                 <div class="divider"></div>
                 <li class="nav-item">
-                    <a href="{{-- route('account.index') --}}"
-                       class="nav-link {{-- request()->routeIs('account.index*') ? 'active' : '' --}}">
+                    <a href="{{-- route('account.index') --}}" class="nav-link {{-- request()->routeIs('account.index*') ? 'active' : '' --}}">
                         <i class="nav-icon fas fa-cog"></i>
                         <p>
                             Account
--- resources/views/satgas/base.blade.php
+++ resources/views/satgas/base.blade.php
@@ -6,6 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Satgas | @yield('title')</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('adminlte/dist/img/icon.png') }}">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
